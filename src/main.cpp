709ff0c904e0b7c97c9860c26a432d04b731dcf7
BZKgenesis <78815908+BZKgenesis@users.noreply.github.com>
2024-12-13 15:48:30 +0100

    change

--- src/main.cpp
+++ src/main.cpp
@@ -3,27 +3,33 @@
 #include <iostream>
 #include<algorithm>
 
-const int H = 720;
-const int W = 1280;
+int H = 720;
+int W = 1280;
 
 //gravité
 const float g = 9.807f;
 
 const int margin = 1;
 
+float zoom = 1.0f;
+sf::Vector2f offset(0, 0);
+
 //taux de rebond
 float damping = 0.5f;
 //calculates dot product of 2 vectors
+sf::Color colorMax = sf::Color(255, 20, 20);
+sf::Color colorMin = sf::Color(20, 20, 255);
+
 inline float dot(const sf::Vector2f& lv, const sf::Vector2f& rv)
 {
     return lv.x * rv.x + lv.y * rv.y;
 }
 
 struct element {
-    element* child1;
-    element* child2;
-    element* child3;
-    element* child4;
+    element* child1 = nullptr;
+    element* child2 = nullptr;
+    element* child3 = nullptr;
+    element* child4 = nullptr;
     sf::RectangleShape rect;
 };
 
@@ -65,6 +71,9 @@ public:
         acc.y = 0;
     }
 };
+
+std::vector<Particle> particulesOrg[10][10];
+std::vector<Particle> particules;
 
 inline sf::Vector2f invert(const sf::Vector2f& source)
 {
@@ -176,13 +185,88 @@ sf::Color lerp(sf::Color a, sf::Color b, float t) {
     return sf::Color(a.r + (b.r - a.r) * t, a.g + (b.g - a.g) * t, a.b + (b.b - a.b) * t);
 }
 
+
+void updateParticules(std::vector<Particle> (& particulesOrg)[10][10], std::vector<Particle>& particules, int nbParticule) {
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			particulesOrg[i][j].clear();
+		}
+	}
+	for (int i = 0; i < particules.size(); i++) {
+		int x = particules[i].pos.x / (W / 10);
+		int y = particules[i].pos.y / (H / 10);
+		//std::cout << x << " " << y << std::endl;
+		particulesOrg[x][y].push_back(particules[i]);
+	}
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+			for (int k = 0; k < particulesOrg[i][j].size(); k++) {
+				particulesOrg[i][j][k].acc += sf::Vector2f(0, g);
+                if (particulesOrg[i][j][k].pos.x < particulesOrg[i][j][k].size + margin) {
+                    particulesOrg[i][j][k].pos.x = particulesOrg[i][j][k].size + margin;
+                }if (particulesOrg[i][j][k].pos.x > W - (particulesOrg[i][j][k].size + margin)) {
+                    particulesOrg[i][j][k].pos.x = W - (particulesOrg[i][j][k].size + margin);
+                }
+                if (particulesOrg[i][j][k].pos.y < particulesOrg[i][j][k].size + margin) {
+                    particulesOrg[i][j][k].pos.y = particulesOrg[i][j][k].size + margin;
+                }if (particulesOrg[i][j][k].pos.y > H - (particulesOrg[i][j][k].size + margin)) {
+                    particulesOrg[i][j][k].pos.y = H - (particulesOrg[i][j][k].size + margin);
+                }
+
+			}
+
+
+			for (int k = i - 1; k < i + 2; k++) {
+                for (int l = j - 1; l < j + 2; l++) {
+                    if (k >= 0 && k < 10 && l >= 0 && l < 10) {
+                        for (int m = 0; m < particulesOrg[k][l].size(); m++) {
+                            for (int n = 0; n < particulesOrg[i][j].size(); n++) {
+                                if (particulesOrg[i][j][n].pos != particulesOrg[k][l][m].pos) {
+                                    if (length(particulesOrg[i][j][n].pos - particulesOrg[k][l][m].pos) < (particulesOrg[i][j][n].size + particulesOrg[k][l][m].size)) {
+										sf::Vector2f mvt(normalise(particulesOrg[i][j][n].pos - particulesOrg[k][l][m].pos) * ((particulesOrg[i][j][n].size + particulesOrg[k][l][m].size) - length(particulesOrg[i][j][n].pos - particulesOrg[k][l][m].pos)) / 2.0f);
+										particulesOrg[i][j][n].pos += mvt;
+										particulesOrg[k][l][m].pos += -mvt;
+										float lengthMvt = length(mvt);
+										//particulesOrg[i][j][n].color = lerp(colorMin, colorMax, lengthMvt);
+										//particulesOrg[k][l][m].color = lerp(colorMin, colorMax, lengthMvt);
+										particulesOrg[i][j][n].color = sf::Color(i*25, j*25, 0);
+										particulesOrg[k][l][m].color = sf::Color(k*25, l*25, 0);
+									}
+								}
+							}
+						}
+					}
+				}
+			}   
+        }
+    }
+
+	particules.clear();
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			for (int k = 0; k < particulesOrg[i][j].size(); k++) {
+				particules.push_back(particulesOrg[i][j][k]);
+			}
+		}
+	}
+}
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(W, H), "Physic");
+    sf::RenderWindow window(sf::VideoMode(W, H), "Physic", sf::Style::Close);
     window.setFramerateLimit(120);
     sf::Clock deltaClock;
+    sf::Vector2f oldPos;
+    bool moving = false;
 
-    std::vector<Particle> particules;
+    float zoom = 1;
+
+    // Retrieve the window's default view
+    sf::View view = window.getDefaultView();
+
+    //std::vector<Particle> particules;
+
 
     int nbParticule = 0;
     int nbParticule_MAX = 1500;
@@ -193,8 +277,6 @@ int main()
     sf::Sound sound;
     sound.setBuffer(buffer);
     //sound.play();
-    sf::Color colorMax = sf::Color(255, 20, 20);
-    sf::Color colorMin = sf::Color(20, 20, 255);
 
     float mvtMax = 0;
 
@@ -205,24 +287,94 @@ int main()
     }
 
 
+   
+
+
 
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
-                window.close();
-            //propulsion autour de la souris when press space
-            if (event.type == sf::Event::KeyPressed && sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
-                for (int i = 0; i < nbParticule; i++) {
-                    int influence = 100;
-                    sf::Vector2f force(0, 0);
-                    sf::Vector2f reltaivePos(particules[i].pos - (sf::Vector2f)sf::Mouse::getPosition(window));
-                    force = normalise(reltaivePos) * (float)(influence - std::fmin(length(reltaivePos), influence)) * 10.0f;
-                    particules[i].acc += force;
+            switch (event.type){
+                case sf::Event::Closed:
+                    window.close(); //propulsion autour de la souris when press space
+                    break;
+                case sf::Event::KeyPressed:
+                    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
+                        for (int i = 0; i < particules.size(); i++) {
+                            int influence = 100;
+                            sf::Vector2f force(0, 0);
+                            sf::Vector2f reltaivePos(particules[i].pos - (sf::Vector2f)window.mapPixelToCoords(sf::Mouse::getPosition(window)));
+                            force = normalise(reltaivePos) * (float)(influence - std::fmin(length(reltaivePos), influence)) * 10.0f;
+                            particules[i].acc += force;
 
+                        }
+                    }
+					break;
+                case sf::Event::Resized:
+                {
+                    // update the view to the new size of the window
+                    sf::FloatRect visibleArea(0, 0, event.size.width, event.size.height);
+                    window.setView(sf::View(visibleArea));
+				    H = event.size.height;
+				    W = event.size.width;
+                    break;
                 }
+                case sf::Event::MouseButtonPressed:
+                {
+                    // Mouse button is pressed, get the position and set moving as active
+                    if (event.mouseButton.button == 2) {
+                        moving = true;
+                        oldPos = window.mapPixelToCoords(sf::Vector2i(event.mouseButton.x, event.mouseButton.y));
+                    }
+                    break;
+                }
+                case  sf::Event::MouseButtonReleased:
+                {
+                    // Mouse button is released, no longer move
+                    if (event.mouseButton.button == 2) {
+                        moving = false;
+                    }
+                    break;
+                }
+                case sf::Event::MouseMoved:
+                {
+                    // Ignore mouse movement unless a button is pressed (see above)
+                    if (!moving)
+                        break;
+                    // Determine the new position in world coordinates
+                    const sf::Vector2f newPos = window.mapPixelToCoords(sf::Vector2i(event.mouseMove.x, event.mouseMove.y));
+                    // Determine how the cursor has moved
+                    // Swap these to invert the movement direction
+                    const sf::Vector2f deltaPos = oldPos - newPos;
+
+                    // Move our view accordingly and update the window
+                    view.setCenter(view.getCenter() + deltaPos);
+                    window.setView(view);
+
+                    // Save the new position as the old one
+                    // We're recalculating this, since we've changed the view
+                    oldPos = window.mapPixelToCoords(sf::Vector2i(event.mouseMove.x, event.mouseMove.y));
+                    break;
+                }
+                case sf::Event::MouseWheelScrolled:
+                    // Ignore the mouse wheel unless we're not moving
+                    if (moving)
+                        break;
+
+                    // Determine the scroll direction and adjust the zoom level
+                    // Again, you can swap these to invert the direction
+                    if (event.mouseWheelScroll.delta <= -1)
+                        zoom = std::min(2.f, zoom + .1f);
+                    else if (event.mouseWheelScroll.delta >= 1)
+                        zoom = std::max(.5f, zoom - .1f);
+
+                    // Update our view
+                    view.setSize(window.getDefaultView().getSize()); // Reset the size
+                    view.zoom(zoom); // Apply the zoom level (this transforms the view)
+                    window.setView(view);
+                    break;
             }
         }
         count++;
@@ -248,10 +400,10 @@ int main()
         for (int i = 0; i < 200; i++) {
             fps_moy += fps_values[i] / 200.0f;
         }
-        window.setTitle("Physic - " + std::to_string(nbParticule) + " - FPS:" + std::to_string(round(fps_moy)));
+        window.setTitle("Physic - " + std::to_string(particules.size()) + " - FPS:" + std::to_string(round(fps_moy)));
         window.clear();
         //collisions
-        for (int i = 0; i < nbParticule; i++) {
+        for (int i = 0; i < particules.size(); i++) {
             /*if ((particules[i].pos.y > H - particules[i].size * 2 && particules[i].vel.y > 0) || (particules[i].pos.y < particules[i].size * 2 && particules[i].vel.y < 0)) {
                 particules[i].vel.y = -particules[i].vel.y * damping;
                 particules[i].vel.x *= damping;
@@ -275,8 +427,11 @@ int main()
                     if (i != j) {
                         if (length(particules[i].pos - particules[j].pos) < (particules[i].size + particules[j].size)) {
                             sf::Vector2f mvt(normalise(particules[i].pos - particules[j].pos) * ((particules[i].size + particules[j].size) - length(particules[i].pos - particules[j].pos)) / 2.0f);
-                            particules[i].pos += mvt;
-                            particules[j].pos += -mvt;
+							if (length(mvt) > 2) {
+                                mvt = normalise(mvt) * 2.0f;
+							}
+                            particules[i].pos += mvt*0.75f;
+                            particules[j].pos += -mvt*0.75f;
                             float lengthMvt = length(mvt);
                             particules[i].color = lerp(colorMin, colorMax, lengthMvt);
                             particules[j].color = lerp(colorMin, colorMax, lengthMvt);
@@ -284,6 +439,8 @@ int main()
                     }
                 }
             }
+
+			//updateParticules(particulesOrg, particules, nbParticule);
 
 
             particules[i].update(deltaT);
